a4ac3b061479fdc0f17fb1752c0b9ab503dd445e
Alessio Carello <alessio.carello@gmail.com>
2022-08-30 07:31:11 +0200

    [FIX] How rss feed is displayed

--- rss.feed.php
+++ rss.feed.php
@@ -13,18 +13,19 @@ $pages = ceil($count / POSTS_PER_PAGE);
  <description><?=DESCRIPTION?></description>
  <link><?=URL?></link>
  <copyright><?=date('Y')?> <?=$_SERVER['HTTP_HOST']?> All rights reserved.</copyright>
- <lastBuildDate><?=$posts[0]['post_timestamp']?></lastBuildDate>
- <pubDate><?=end($posts)['post_timestamp']?></pubDate>
  <ttl>1800</ttl>
  <?php
     foreach($posts as $post) {
         $content = trim(strip_tags($post['post_content']));
+        $direct_link = URL . '/posts/' . $post['id'];
 ?>
 <item>
-  <title><?=substr($content, 0, 32)?></title>
-  <description><?=$content?></description>
-  <link><?=URL?></link>
-  <guid isPermaLink="false"><?='POST_#' . $post['id']?></guid>
+  <title><?=substr($content, 0, 32)?></title>  
+  <description>
+  <![CDATA[ <img src="https://alessiocarello.tk/mb/assets/logo.png" /><a href="<?=$direct_link?>" target="_blank"><?=$content?></a> ]]>
+  </description>
+  <link><?=$direct_link?></link>
+  <guid isPermaLink="true"><?='POST_#' . $post['id']?></guid>
   <pubDate><?=date('c', $post['post_timestamp'])?></pubDate>
  </item> 
 <?php } ?>
